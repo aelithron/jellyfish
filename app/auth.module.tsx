97634386e8c0a16ff1abc826678750499b912127
Nova <nova@novatea.dev>
2026-06-30 14:41:29 -0600

    add more auth code, split stages properly

--- app/auth.module.tsx
+++ app/auth.module.tsx
@@ -1,19 +1,53 @@
 "use client";
 import jellyfin from "@/utils/jellyfin";
 import { useState } from "react";
+import { Api } from "@jellyfin/sdk";
+import { getUserApi } from "@jellyfin/sdk/lib/utils/api/user-api";
+import { UserDto } from "@jellyfin/sdk/lib/generated-client/models";
 
 export function SignIn() {
+  const [jfAPI, setJFAPI] = useState<Api | null>(null);
   const [stage, setStage] = useState<"server" | "credentials">("server");
   const [addr, setAddr] = useState<string>("");
-  async function handleSubmit(e: React.SubmitEvent) {
+  const [publicUsers, setPublicUsers] = useState<UserDto[]>([]);
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  async function loadServer(e: React.SubmitEvent) {
     e.preventDefault();
     const server = jellyfin.discovery.findBestServer(await jellyfin.discovery.getRecommendedServerCandidates(addr));
+    if (!server) {
+      alert("This server couldn't be found! Please check your server address.");
+      return;
+    }
+    const api = jellyfin.createApi(addr);
+    setAddr(server.address);
+    setJFAPI(api);
+    setPublicUsers((await getUserApi(api!).getPublicUsers()).data);
+    setStage("credentials");
+  }
+  async function authenticate(e: React.SubmitEvent) {
+    e.preventDefault();
   }
   return (
-    <form onSubmit={handleSubmit} className="flex flex-col gap-2">
-      <label htmlFor="server">Server</label>
-      <input value={addr} onChange={(e) => setAddr(e.target.value)} id="server" className="border-2 border-black bg-slate-500 text-black p-1 rounded-xl" />
-      <button type="submit" className="hover:text-sky-500">Continue</button>
-    </form>
+    <div>
+      {stage === "server" && <form onSubmit={loadServer} className="flex flex-col gap-2">
+        <label htmlFor="server">Server</label>
+        <input value={addr} onChange={(e) => setAddr(e.target.value)} id="server" className="border-2 border-black bg-slate-500 text-black p-1 rounded-xl" />
+        <button type="submit" className="hover:text-sky-500">Continue</button>
+      </form>}
+      {stage === "credentials" && <form onSubmit={authenticate} className="flex flex-col gap-2">
+        <div className="flex flex-col gap-2">
+          <label htmlFor="username">Username</label>
+          <input value={username} onChange={(e) => setUsername(e.target.value)} id="username" className="border-2 border-black bg-slate-500 text-black p-1 rounded-xl" />
+          <div className="flex gap-1">
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            {publicUsers.map((user) => (<button key={user.Id} type="button" onClick={() => setUsername(user.Name!)} className="bg-slate-700 rounded-xl py-1 px-2 flex gap-1 items-center text-center justify-center"><img src={`${addr}/Users/${user.Id}/Images/Primary`} alt="Jellyfin PFP" className="h-6 w-6" /> {user.Name}</button>))}
+          </div>
+        </div>
+        <label htmlFor="password">Password</label>
+        <input value={password} onChange={(e) => setPassword(e.target.value)} id="password" className="border-2 border-black bg-slate-500 text-black p-1 rounded-xl" />
+        <button type="submit" className="hover:text-sky-500">Sign In</button>
+      </form>}
+    </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,11 @@ export default function Home() {
   return (
     <main className="flex flex-col min-h-screen">
       <div className="grid grid-cols-1 grid-rows-2 md:grid-cols-2 md:grid-rows-1">
-        <div className="flex flex-col text-center items-center justify-center bg-sky-900 min-h-screen p-8 md:p-20 gap-2">
+        <div className="flex flex-col text-center items-center justify-center bg-sky-900 md:min-h-screen p-8 md:p-20 gap-2">
           <h1 className="text-2xl font-semibold">Jellyfish</h1>
           <p>&quot;Fish up&quot; media (music, movies, tv shows, etc) from a Jellyfin server!</p>
         </div>
-        <div className="flex flex-col text-center items-center justify-center min-h-screen p-8 md:p-20 gap-2">
+        <div className="flex flex-col text-center items-center justify-center md:min-h-screen p-8 md:p-20 gap-2">
           <h1 className="text-xl">Sign in</h1>
           <p>(you need a Jellyfin server to use this!)</p>
           <SignIn />
